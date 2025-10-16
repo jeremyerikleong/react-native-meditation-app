08b4fd8f1b65837cfe5e89cfc0a677ba10c1a178
Jeremy <jeremythegreat95@gmail.com>
2025-10-16 10:20:53 +0800

    setup nativewind

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -3,7 +3,7 @@ import { View, Text } from 'react-native';
 export default function HomeScreen() {
     return (
         <View>
-            <Text>Home Screen</Text>
+            <Text className="text-xl font-bold text-blue-500">Home Screen</Text>
         </View>
     )
 }
